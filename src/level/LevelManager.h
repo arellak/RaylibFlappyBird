55f73ce107a85b5e80d582cbc4648a177437c5e2
nimbl0 <nimble0@protonmail.com>
2021-05-05 16:50:02 +0200

    implemented camera

---
+++ src/level/LevelManager.h
@@ -0,0 +1,17 @@
+//
+// Created by rocketman on 14.04.21.
+//
+
+#ifndef RAYLIBB_LEVELMANAGER_H
+#define RAYLIBB_LEVELMANAGER_H
+
+#include <vector>
+#include "Level.h"
+
+class LevelManager {
+public:
+    static inline std::vector<Level> levels;
+};
+
+
+#endif //RAYLIBB_LEVELMANAGER_H
